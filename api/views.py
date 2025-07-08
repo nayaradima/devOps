d932b68fdd6cd9d23b87d9b20763cbec59a59496
nayara_dima <nayaradima@gmail.com>
2025-07-08 16:41:12 +0100

    feat: Updating new gethub action workflow

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+# Book Resource
+# /api/books
+
+class BookView(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response(
+            {
+                "hello":"django"
+            }
+        )
+        
+        
+book_view = BookView.as_view()
+
